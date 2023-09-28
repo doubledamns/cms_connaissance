bbc290bef72177931e40fa6068e8102b3fc09d74
doubledamns <145429185+doubledamns@users.noreply.github.com>
2023-09-28 14:29:07 +0200

    backend form login
    ok

--- admin/index.php
+++ admin/index.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BDC WordPress</title>
     <link href="../css/admin.css" rel="stylesheet"/>
+      <!--Tailwinds-->
+    <script src="https://cdn.tailwindcss.com"></script>
     <script src="../js/default.js"></script>
     <script>
     tailwind.config = {
@@ -22,7 +24,9 @@
 <h1 class="text-3xl font-bold underline">
     Hello world!
 </h1>
+
 <?php
+  // check if the user is connected
   if(!isset($_COOKIE["is_connected"])) {
     require("./admin.php");
   } else {
---
+++ include/fonctions.php
@@ -0,0 +1,18 @@
+<?php
+
+function connectToBase(){
+    """
+        Connexion à la bdd
+    """
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = "";
+    $dbname = "cms_bdd";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);    
+}
+
+}?> 
