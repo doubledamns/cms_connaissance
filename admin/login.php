6a5fcb560364e24c5efd6fbbb590db4a4f1c2062
Abdelkerim Mahamat Adef <abdelkerimmahamatadef@gmail.com>
2023-09-28 16:48:01 +0200

    modif css login
    modif css et js

--- admin/login.php
+++ admin/login.php
@@ -3,17 +3,31 @@
 
 <head>
     <?php require('../components/head.php') ?>
+    <link rel="stylesheet" href="../css/admin.css">
+    
 </head>
 
 <body>
-    <form action="auth.php" id="clickform" method="post">
-        <label><span class='utili'>Nom d'utilisateur:</span></label><br />
-        <input type="text" name="email" id="email"><br />
-        <label for="password"><span class='mp'>Mot de passe:</span></label><br />
-        <input type="password" name="password" id="password">
-        <pre></pre>
-        <button type="submit">Connexion</button>
-    </form>
+    <!-- <form action="auth.php" id="clickform" method="post"> -->
+        <!-- <label><span class='utili'>Nom d'utilisateur:</span></label><br /> -->
+        <!-- <input type="text" name="email" id="email"><br /> -->
+        <!-- <label for="password"><span class='mp'>Mot de passe:</span></label><br /> -->
+        <!-- <input type="password" name="password" id="password"> -->
+        <!-- <pre></pre> -->
+        <!-- <button type="submit">Connexion</button> -->
+    <!-- </form> -->
+
+ <form action="auth.php" id="clickform" method="POST">
+ <h1>Connexion</h1>
+ 
+ <label><b>Nom d'utilisateur</b></label>
+ <input type="text" id="email" placeholder="Entrer le nom d'utilisateur" name="username">
+
+ <label><b>Mot de passe</b></label>
+ <input type="password" id="password" placeholder="Entrer le mot de passe" name="password">
+
+ <input type="submit" id='submit' value='LOGIN' ></form>
+ <script src="../js/default.js"></script> 
 
     <?php require('../components/injectionsScript.php') ?>
 </body>
