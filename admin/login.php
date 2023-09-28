d537dd4c5f7d3a0d1e61c29603f12b216e4e4cd7
Alice GUEGAN <91895630+gueganalice@users.noreply.github.com>
2023-09-28 16:04:31 +0200

    MAJ pages

--- admin/login.php
+++ admin/login.php
@@ -2,35 +2,20 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BDC WordPress</title>
-
-    <!--Favicons-->
-    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
-    <link rel="manifest" href="favicon/site.webmanifest">
-    <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#ffffff">
-
-    <!--Tailwinds-->
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="../css/admin.css">
-    <script src="../js/default.js"></script>
-  
+    <?php require('../components/head.php') ?>
 </head>
 
 <body>
-<form action="auth.php" id="clickform" method="post"> 
-	<label ><span class='utili'>Nom d'utilisateur:</span></label><br/>
-	<input type="text" name="email" id="email" ><br/>
-	<label for="password"><span class='mp'>Mot de passe:</span></label><br/>
-	<input type="password" name="password" id="password">
-	<pre></pre>
-	<button type="submit">Connexion</button>
-</form>  
+    <form action="auth.php" id="clickform" method="post">
+        <label><span class='utili'>Nom d'utilisateur:</span></label><br />
+        <input type="text" name="email" id="email"><br />
+        <label for="password"><span class='mp'>Mot de passe:</span></label><br />
+        <input type="password" name="password" id="password">
+        <pre></pre>
+        <button type="submit">Connexion</button>
+    </form>
+
+    <?php require('../components/injectionsScript.php') ?>
 </body>
 
 </html>
