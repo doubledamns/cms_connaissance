ca7679bd426b09034c4b2f0ad3d7b4241c6f7cfc
camilntl <91890013+camilntl@users.noreply.github.com>
2023-11-06 11:31:36 +0100

    upload img

--- pages/creerPage.php
+++ pages/creerPage.php
@@ -11,7 +11,7 @@
     <div class="p-4 sm:ml-64">
         <section id="creerPage">
             <h2 class="pb-12 text-4xl font-semibold text-center md:text-left">Créer une page</h2>
-            <form id="createForm" method="post" action="../submit.php">
+            <form id="createForm" method="post" action="../submit.php" enctype="multipart/form-data">
                 <div class="space-y-6">
                     <div class="sm:col-span-3 shadow-lg p-4 rounded-lg w-fit">
                         <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">Titre de la page</label>
@@ -32,10 +32,12 @@
                                     <path fill-rule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clip-rule="evenodd" />
                                 </svg>
                                 <div class="mt-4 flex text-sm leading-6 text-gray-600">
-                                    <label for="file-upload" class="relative cursor-pointer rounded-md bg-white font-semibold text-purple-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
+                                    <!--<label for="image" class="relative cursor-pointer rounded-md bg-white font-semibold text-purple-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
                                         <span>Télécharges une photo</span>
-                                        <input id="file-upload" name="file-upload" type="file" class="sr-only">
-                                    </label>
+                                        <input id="image" name="image" type="file" class="sr-only">
+                                    </label>-->
+                                    <label for="img">Choose a file:</label>
+                                    <input type="file" id="img" name="img">
                                     <p class="pl-1">ou fais la glisser</p>
                                 </div>
                                 <p class="text-xs leading-5 text-gray-600">PNG, JPG, GIF jusqu'à 10MB</p>
--- submit.php
+++ submit.php
@@ -19,9 +19,37 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $title = $_POST["title"];
     $content = $_POST["content"];
     
+    // Initialize a variable for the image path
+    $imagePath = '';
+
+    // Check and process the uploaded file
+    if (isset($_FILES["img"]) && $_FILES["img"]["error"] == 0) {
+        echo "1";
+        $allowed_types = array("image/jpeg", "image/png", "image/gif");
+        if (in_array($_FILES["img"]["type"], $allowed_types)) {
+            $target_dir = "images/";echo "3";
+            $target_file = $target_dir . basename($_FILES["img"]["name"]);
+
+
+            
+            // Move the uploaded file to the target directory
+            if (move_uploaded_file($_FILES["img"]["tmp_name"], $target_file)) {
+                echo "2";
+                $imagePath = $target_file;
+                
+            } else {
+                echo "There was an error uploading your file.";
+            }
+        } else {
+            echo "Error: Only JPG, PNG, and GIF files are allowed.";
+        }
+    }
+
+    echo"8";
+    
     // Prepare the SQL query
-    if ($stmt = $conn->prepare("INSERT INTO page (title, content) VALUES (?, ?)")) {
-        $stmt->bind_param("ss", $title, $content);
+    if ($stmt = $conn->prepare("INSERT INTO page (title, content, img) VALUES (?, ?, ?)")) {
+        $stmt->bind_param("sss", $title, $content, $imagePath);
         
         // Execute the SQL query
         if ($stmt->execute()) {
