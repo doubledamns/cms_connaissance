2fab10f9dac50c8edd0459021a990627ad813b46
Alice GUEGAN <91895630+gueganalice@users.noreply.github.com>
2023-11-06 11:46:36 +0100

    changelent titre

--- admin/titreSite.php
+++ admin/titreSite.php
@@ -12,9 +12,9 @@
         <section id="titleSite">
             <h2 class="pb-12 text-4xl font-semibold text-center md:text-left">Changer le nom du site</h2>
             <div class="sm:col-span-3 shadow-lg p-4 rounded-lg w-fit">
-                <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">Nom du site</label>
+                <label for="nom-site" class="block text-sm font-medium leading-6 text-gray-900">Nom du site</label>
                 <div class="mt-2">
-                    <input placeholder="Nom du site" type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-56 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-300 sm:text-sm sm:leading-6">
+                    <input placeholder="Nom du site" type="text" name="nom-site" id="nom-site" autocomplete="given-name" class="block w-56 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-300 sm:text-sm sm:leading-6">
                 </div>
 
             </div>
@@ -26,6 +26,37 @@
     </div>
 
     <?php require('../components/injectionsScript.php') ?>
+
+    <?php
+    // Récupérez le nouveau nom du site depuis le formulaire
+    $nomDuSite = $_POST['nom-site'];
+
+    // Établissez une connexion à la base de données (assurez-vous de configurer vos paramètres de connexion)
+    $servername = "votre_hôte";
+    $username = "votre_nom_utilisateur";
+    $password = "votre_mot_de_passe";
+    $dbname = "votre_base_de_données";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Vérifiez la connexion
+    if ($conn->connect_error) {
+        die("La connexion à la base de données a échoué : " . $conn->connect_error);
+    }
+
+    // Préparez et exécutez la requête d'insertion
+    $sql = "INSERT INTO noms_sites (nom) VALUES ('$nomDuSite')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Le nom du site a été enregistré avec succès dans la base de données.";
+    } else {
+        echo "Erreur lors de l'enregistrement du nom du site : " . $conn->error;
+    }
+
+    // Fermez la connexion à la base de données
+    $conn->close();
+    ?>
+
 </body>
 
 </html>
