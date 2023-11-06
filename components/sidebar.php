147a7b52efdb64f8d9b28183a0fc584245e7cd7d
Alice GUEGAN <91895630+gueganalice@users.noreply.github.com>
2023-11-06 16:33:52 +0100

    gad

--- components/sidebar.php
+++ components/sidebar.php
@@ -47,9 +47,9 @@ $result = $stmt->get_result();
 
 <aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
     <div class="h-full px-3 py-4 overflow-y-auto bg-purple-200 ">
-        <div class="flex flex-col justify-center w-full h-20 mb-6">
-
-
+    <div class="flex flex-col justify-center w-full h-20 mb-6">
+            <img class="w-20 h-full self-center" src="/images/logo-BDC.png" alt="Logo BDC" />
+            <h1 class="text-xl font-semibold text-center"><?php echo $nomDuSite; ?></h1>
         </div>
         <ul class="space-y-2 font-medium">
             <li>
