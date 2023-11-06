4d148afcf1fcd2fae53c5271a94d0d9b1e6a12b7
camilntl <91890013+camilntl@users.noreply.github.com>
2023-11-06 17:47:13 +0100

    j

--- admin/listePage.php
+++ admin/listePage.php
@@ -1,6 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php
+// Paramètres de connexion à la base de données
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "cms_bdd";
 
+// Établir la connexion à la base de données
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Vérifier la connexion
+if ($conn->connect_error) {
+    die("Échec de la connexion : " . $conn->connect_error);
+}
+
+
+// Récupération du nom du site
+$stmt = $conn->prepare('SELECT nom FROM noms_sites WHERE id = 1');
+if ($stmt->execute()) {
+    $stmt->bind_result($nomDuSite);
+
+    $stmt->fetch();
+}
+$stmt->close(); // Fermeture de la première requête préparée
+
+// Récupération des pages
+$stmt = $conn->prepare("SELECT id, title FROM page");
+
+$stmt->execute();
+$result = $stmt->get_result();
+?>
 <head>
     <?php require('../components/head.php'); ?>
 </head>
@@ -15,6 +45,26 @@
             </div>
         </section>
     </div>
+
+<table>
+<thead>
+    <tr>
+    <?php if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    echo '
+                                                                        <th> href="../admin/modifPage.php?page_id=' . $row["id"] . '" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">' . htmlspecialchars($row["title"]) . '</th>';
+                }
+            } else {
+                echo "Aucune page trouvée.";
+            }
+            $stmt->close(); // Fermeture de la deuxième requête préparée
+
+            // Fermeture de la connexion
+            $conn->close(); ?>
+              </tr>
+  </thead>
+</table>
+
 
     <?php require('../components/injectionsScript.php') ?>
 </body>
