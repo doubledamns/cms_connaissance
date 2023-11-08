0f54ac40189288f5d974068823093c37daaa61a0
Alice GUEGAN <91895630+gueganalice@users.noreply.github.com>
2023-11-08 16:32:13 +0100

    men

--- admin/site.php
+++ admin/site.php
@@ -11,10 +11,21 @@
     <?php require('../components/sidebar.php'); ?>
 
     <?php require('../components/sidebar_sous_site.php'); ?>
-    <div class="ml-96 pl-36">
-        <div class="w-52 h-52">
-            <img class="w-full h-full" src="<?php echo $urlDuSite?>" alt="Logo BDC">
-            <h2><?php echo $nomDuSite?></h2>
+    <div class="pl-72 sm:ml-64 flex flex-col">
+        <div class="flex flex-row">
+            <div class="w-52 h-52">
+                <img class="w-full h-full" src="<?php echo $urlDuSite ?>" alt="Logo BDC" />
+            </div>
+            <h2 class="self-center">
+                <?php echo $nomDuSite ?>
+            </h2>
+        </div>
+        <div class="flex">
+            <p>élément 1</p>
+            <p>élément 2</p>
+            <p>élément 3</p>
         </div>
     </div>
-</body></html>
+</body>
+
+</html>
